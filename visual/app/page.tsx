e6b4ec87296276d6c16c15f308d782863e52059c
Pilartri <167590609+Pilartri@users.noreply.github.com>
2024-10-22 12:21:58 -0300

    cambios 2

--- visual/app/page.tsx
+++ visual/app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-24"></div>
         <Image className='ml-[550px]'
-          src="/ICONO_JJOO.png" 
+          src="/iconojjoo.png" 
           alt="Olympic Women Participation"
           width={200}
           height={300}
